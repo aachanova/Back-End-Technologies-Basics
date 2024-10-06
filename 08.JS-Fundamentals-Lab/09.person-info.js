1da54273acfe45b5927aa81a5c59ea8d08c5911e
Ani <anelia_9@yahoo.com>
2024-10-06 16:09:42 +0300

    created 09.person-info.js

---
+++ 08.JS-Fundamentals-Lab/09.person-info.js
@@ -0,0 +1,12 @@
+function solve(firstName, lastName, age) {
+    const person = {
+        firstName,
+        lastName,
+        age
+    }
+
+    return person;
+}
+
+console.log(solve("Peter", "Pan", "20"));
+console.log(solve("George", "Smith", "18"));
