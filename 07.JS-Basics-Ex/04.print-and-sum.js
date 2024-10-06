ce019dc2a2fe3968f4db9372008bcb65c16c2d3c
Ani <anelia_9@yahoo.com>
2024-10-06 09:42:37 +0300

    transformed input as array

--- 07.JS-Basics-Ex/04.print-and-sum.js
+++ 07.JS-Basics-Ex/04.print-and-sum.js
@@ -1,4 +1,6 @@
-function solve(start, end) {
+function solve(arr) {
+    let start = Number(arr[0]);
+    let end = Number(arr[1]);
     let sum = 0; 
     let nums = '';
     for (let i = start; i <= end; i++) {
@@ -10,6 +12,6 @@ function solve(start, end) {
     console.log(`Sum: ${sum}`);
 }
 
-solve(5, 10);
-solve(0, 26);
-solve(50, 60);
+solve([5, 10]);
+solve([0, 26]);
+solve([50, 60]);
